21397775817302d7d162e541c2fea97bb1d0a958
Cephas Zulu <48860145+cazterk@users.noreply.github.com>
2023-06-07 12:08:28 +0200

    add icons to language and frameworks section

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import { IconContext } from "react-icons";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
 
 const Projects = () => {
-  const paraProperties = "py-1 text-sm font-semibold";
+  const paraProperties = " flex py-1 text-sm font-semibold";
 
   return (
     <div className="h-fit sm:h-screen">
@@ -9,23 +12,51 @@ const Projects = () => {
         <div className="skills h-full border-solid border-2 border-gray-200 rounded">
           <h1 className="p-4 font-bold">SKill</h1>
           <div className="content p-4">
-            <p className={paraProperties}>
-              ⚛️React econsystem [JavaScript, TypeScript and more]
-            </p>
+            <ul className="list-disc">
+              <li className={paraProperties}>
+                Very Good At Frontend Development and UI-Engineering
+              </li>
 
-            <p className={paraProperties}>
-              {" "}
-              💻UI-Engineering [Complex CSS and Design Systems]
-            </p>
-            <p className={paraProperties}> 🛠️Dotnet ecosystem [C#]</p>
-            <p className={paraProperties}>
-              {" "}
-              📱Android Development [Kotlin with Jetpack Compose ]
-            </p>
-            <p className={paraProperties}>🛳️Docker containerization</p>
-            <p className={paraProperties}>
-              🎮Game Development [Unity 3d Engine with C#]
-            </p>
+              <li className={paraProperties}> Backend Development OOP</li>
+              <li className={paraProperties}> Database Management</li>
+              <li className={paraProperties}>
+                {" "}
+                Mobile With Android Development
+              </li>
+              <li className={paraProperties}>Containerization</li>
+              <li className={paraProperties}>Game Development</li>
+            </ul>
+            <h6 className=" pt-4 font-bold">Languages & Frameworks</h6>
+
+            <IconContext.Provider
+              value={{
+                size: "20",
+                color: "#ff6b6b",
+                style: { verticalAlign: "middle", marginRight: "2px" },
+              }}
+            >
+              <p className={paraProperties}>
+                <FaIcons.FaLaptopCode />
+                Frontend: HTML, CSS, JavaScript, Typescript, React
+              </p>
+
+              <p className={paraProperties}>
+                <AiIcons.AiFillCode />
+                Backend: C#, Kotlin, Dotnet, Ktor
+              </p>
+              <p className={paraProperties}>
+                <FaIcons.FaAndroid />
+                Mobile: Android Studio + Jetpack Compose
+              </p>
+              <p className={paraProperties}>
+                <FaIcons.FaDatabase />
+                Databases: PostgreSQL, Ms-SQL, MongoDB
+              </p>
+              <p className={paraProperties}>
+                <AiIcons.AiOutlineContainer />
+                Containerization & Orchestration: Docker
+              </p>
+            </IconContext.Provider>
           </div>
         </div>
         <div className="hobbies h-full border-solid border-2 border-gray-200 rounded">
